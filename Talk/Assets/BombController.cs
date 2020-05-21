30254ef8f3474eef614dd8f7ed03ba7ee0680617
Krzysztof Maliński <malinakrzychu@gmail.com>
2020-05-21 20:24:34 +0200

    Basic Project

---
+++ Talk/Assets/BombController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class BombController : MonoBehaviour
+{
+    [SerializeField] private int _pointsToDecrease = 0;
+    public int Points => _pointsToDecrease;
+
+    private void Start()
+    {
+        Random random = new Random();
+        _pointsToDecrease = random.Next(1, 10);
+    }
+}
